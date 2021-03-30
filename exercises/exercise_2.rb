1d601f7001ad773b3d8013237f8ba805f9c58a5b
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-30 16:06:54 +0000

    completed ex.2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store1.update(name: "Kitsilano")
+
+@store2 = Store.where(id: 2)
+
+p @store1
+p @store2
