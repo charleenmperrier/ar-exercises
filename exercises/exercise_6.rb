69a93784b9daab4ff3ff13e70544e58c708bd419
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-30 17:33:10 +0000

    completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,22 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "K", last_name: "V", hourly_rate: 6)
+@store1.employees.create(first_name: "m", last_name: "i", hourly_rate: 600)
+@store1.employees.create(first_name: "Km", last_name: "Vi", hourly_rate: 50)
+
+
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 50)
+@store2.employees.create(first_name: "Kh", last_name: "Vii", hourly_rate: 50)
+@store2.employees.create(first_name: "am", last_name: "ni", hourly_rate: 60)
+@store2.employees.create(first_name: "Krram", last_name: "Viri", hourly_rate: 60)
+
